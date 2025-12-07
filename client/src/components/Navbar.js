80ea21d8f7af1f8a6d04a7ab544ef4ba656f74e0
Iram Ridwan <ridwaniram@gmail.com>
2025-12-08 01:39:44 +0600

    Clean up Navbar links by removing unused features
    Removed unused NavLink components for Feature 3 and Feature 5.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -28,18 +28,11 @@ export default function Navbar() {
         {/* --- Rahima Alam (M3) - FIXED LINK --- */}
         
         <NavLink to="/feature-3" className={getLinkClass}>Career Assessment </NavLink>
-         {/* --- Yousuf--- */}
-        <NavLink to="/feature-3" className={getLinkClass}>
-          Feature 3 (M3)
-        </NavLink>
+  
         <NavLink to="/feature-4" className={getLinkClass}>
           Feature 4 (M4)
         </NavLink>
-        <NavLink to="/feature-5" className={getLinkClass}>
-          Feature 5 (M5)
-        </NavLink>
       </div>
-      
       <div className="flex space-x-3">
         <NavLink to="/login" className="px-3 py-2 rounded-md text-sm font-bold bg-accent text-primary-bg hover:bg-accent-hover transition-all">
           Login / Sign Up
